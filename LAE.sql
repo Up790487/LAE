9960886732b73225e785a2493fd29ade2753b17a
RidwanAbiolaTw <36053482+RidwanAbiolaTw@users.noreply.github.com>
2018-03-03 19:52:27 +0000

    LAE.sql

--- LAE.sql
+++ LAE.sql
@@ -1,1 +1,96 @@
 
+Create databse LAE-dB;
+
+  create table Nightlife (
+    Sector 
+    Establishment name Varchar(10) not null, 
+    Description Varchar(10) not null, 
+    Business Hours
+    Picture:
+    Reviews 
+    Longitude int(10) not null,
+    Latitude int(10) not null
+
+  );
+  create table Restaurants (
+    Sector 
+    Establishment name Varchar(10) not null, 
+    Description Varchar(20) not null, 
+    Business Hours
+    Picture:
+    Reviews
+    Longitude int(10) not null,
+    Latitude int(10) not null,
+  );
+  
+  create table Bar/Pubs (
+    Sector 
+    Establishment name Varchar(10) not null, 
+    Description Varchar(10) not null, 
+    Business Hours
+    Picture:
+    Reviews
+    Longitude int(10) not null,
+    Latitude int(10) not null,
+  );
+  
+  create table Cinemas/Theatre (
+     Sector  
+    Establishment name Varchar(10) not null, 
+    Description Varchar(10) not null, 
+    Business Hours
+    Picture:
+    Reviews
+    Longitude int(10) not null,
+    Latitude int(10) not null,
+  );
+  create table Museums (
+     Sector 
+    Establishment name Varchar(10) not null, 
+    Description Varchar(10) not null, 
+    Business Hours
+    Picture:
+    Reviews
+    Longitude int(10) not null,
+    Latitude int(10) not null,
+  );
+  create table Sport (
+     Sector 
+    Establishment name Varchar(10) not null, 
+    Description Varchar(10) not null, 
+    Business Hours
+    Picture:
+    Reviews
+    Longitude int(10) not null,
+    Latitude int(10) not null,
+  );
+  create table Parks (
+     Sector 
+    Establishment name Varchar(10) not null, 
+    Description Varchar(10) not null, 
+    Business Hours
+    Picture:
+    Reviews
+    Longitude int(10) not null,
+    Latitude int(10) not null,
+  );
+  create table Landmarks/POI (
+     Sector 
+    Establishment name Varchar(10) not null, 
+    Description Varchar(10) not null, 
+    Business Hours
+    Picture:
+    Reviews
+    Longitude int(10) not null,
+    Latitude int(10) not null,
+  );
+  create table Transport (
+    Sector 
+    Establishment name Varchar(10) not null, 
+    Description Varchar(10) not null, 
+    Business Hours
+    Picture:
+    Reviews
+    Longitude int(10) not null,
+    Latitude int(10) not null,
+  );
