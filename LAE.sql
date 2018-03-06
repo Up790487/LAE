cf5404840a55842e709cc15d64e1f7137662e8cd
RIDWAN ABIOLA TW <36053482+RidwanAbiolaTw@users.noreply.github.com>
2018-03-06 13:49:46 +0000

    Update LAE.sql

--- LAE.sql
+++ LAE.sql
@@ -3,94 +3,94 @@ Create databse LAE-dB;
 
   create table Nightlife (
     Sector 
-    Establishment name Varchar(10) not null, 
-    Description Varchar(10) not null, 
-    Business Hours
+    Establishment name Varchar(20) not null, 
+    Description Varchar(40) not null, 
+    Business Hours Time
     Picture jpg 
-    Reviews 
+    Reviews Varchar (100) not null
     Longitude int(10) not null,
     Latitude int(10) not null
 
   );
   create table Restaurants (
     Sector 
-    Establishment name Varchar(10) not null, 
-    Description Varchar(20) not null, 
-    Business Hours
+    Establishment name Varchar(20) not null, 
+    Description Varchar(40) not null, 
+    Business Hours Time
     Picture jpg
-    Reviews
+    Reviews Varchar (100) not null
     Longitude int(10) not null,
     Latitude int(10) not null,
   );
   
   create table Bar/Pubs (
     Sector 
-    Establishment name Varchar(10) not null, 
-    Description Varchar(10) not null, 
-    Business Hours
+    Establishment name Varchar(20) not null, 
+    Description Varchar(40) not null, 
+    Business Hours Time
     Pictur jpg
-    Reviews
+    Reviews Varchar (100) not null
     Longitude int(10) not null,
     Latitude int(10) not null,
   );
   
   create table Cinemas/Theatre (
      Sector  
-    Establishment name Varchar(10) not null, 
-    Description Varchar(10) not null, 
-    Business Hours
+    Establishment name Varchar(20) not null, 
+    Description Varchar(40) not null, 
+    Business Hours Time
     Picture jpg
-    Reviews
+    Reviews Varchar (100) not null
     Longitude int(10) not null,
     Latitude int(10) not null,
   );
   create table Museums (
      Sector 
-    Establishment name Varchar(10) not null, 
-    Description Varchar(10) not null, 
-    Business Hours
+    Establishment name Varchar(20) not null, 
+    Description Varchar(40) not null, 
+    Business Hours Time
     Picture jpg
-    Reviews
+    Reviews Varchar (100) not null
     Longitude int(10) not null,
     Latitude int(10) not null,
   );
   create table Sport (
      Sector 
-    Establishment name Varchar(10) not null, 
-    Description Varchar(10) not null, 
-    Business Hours
+    Establishment name Varchar(20) not null, 
+    Description Varchar(40) not null, 
+    Business Hours Time
     Picture jpg
-    Reviews
+    Reviews Varchar (100) not null
     Longitude int(10) not null,
     Latitude int(10) not null,
   );
   create table Parks (
      Sector 
-    Establishment name Varchar(10) not null, 
-    Description Varchar(10) not null, 
-    Business Hours
+    Establishment name Varchar(20) not null, 
+    Description Varchar(40) not null, 
+    Business Hours Time
     Picture jpg
-    Reviews
+    Reviews Varchar (100) not null
     Longitude int(10) not null,
     Latitude int(10) not null,
   );
   create table Landmarks/POI (
      Sector 
-    Establishment name Varchar(10) not null, 
-    Description Varchar(10) not null, 
-    Business Hours
+    Establishment name Varchar(20) not null, 
+    Description Varchar(40) not null, 
+    Business Hours Time
     Picture jpg
-    Reviews
+    Reviews Varchar (100) not null
     Longitude int(10) not null,
     Latitude int(10) not null,
   );
   create table Transport (
     Sector 
-    Establishment name Varchar(10) not null, 
-    Description Varchar(10) not null, 
-    Business Hours
+    Establishment name Varchar(20) not null, 
+    Description Varchar(40) not null, 
+    Business Hours Time
     Picture jpg
-    Reviews
+    Reviews Varchar (100) not null
     Longitude int(10) not null,
     Latitude int(10) not null,
   );
